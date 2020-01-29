1bb4c8239229192e12f8f7d7911800817bbd6b63
Colin de Vries <colindevries325@gmail.com>
2020-01-29 13:08:00 -0800

    completed insert and contains methods for binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
+# from dll_stack import Stack
+# from dll_queue import Queue
 import sys
 sys.path.append('../queue_and_stack')
-from dll_queue import Queue
-from dll_stack import Stack
 
 
 class BinarySearchTree:
@@ -12,12 +12,35 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
-
+        if value < self.value:
+            if self.left is None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        elif value >= self.value:
+            if self.right is None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
     # Return True if the tree contains the value
     # False if it does not
+
     def contains(self, target):
-        pass
+        if self.value == target:
+            return True
+        else:
+            if target >= self.value:
+                if self.right:
+                    return self.right.contains(target)
+                else:
+                    return False
+            elif target < self.value:
+                if self.left:
+                    return self.left.contains(target)
+                else:
+                    return False
+            elif self.value == None:
+                return False
 
     # Return the maximum value found in the tree
     def get_max(self):
